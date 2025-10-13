4223a2f9af07530e7fcf03998bd70cb6940567c4
Pauovidi <83812648+Pauovidi@users.noreply.github.com>
2025-10-13 10:12:26 +0200

    Update and rename next.config.js to next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // añade aquí lo que ya tuviera tu config: images, headers, rewrites, etc.
+};
+
+export default nextConfig;
